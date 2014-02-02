50d0a6ae286405ef31b8e60c784dea33104ca3c9
treeherder <b.d.reddybest@gmail.com>
2014-02-02 05:28:24 -0800

    crazy output

--- engine.py
+++ engine.py
@@ -1,2 +1,21 @@
+import sqlite3
 import bot
-stream.filter(track=['year luigi'])
+from multiprocessing import Process
+
+def strm():
+  stream.filter(track=['year luigi'])
+
+def prnt():
+  c = sqlite3.connect('waluigi_tt.db')
+  cur = c.cursor()
+  cur.execute("SELECT user,status from tweets order by user, limit 1") 
+  r = cur.fetchone()
+  print "content {0}".format(r)
+
+while True:
+  proc_1 =  Process(target=strm)
+  proc_2 = Process(target=prnt)
+  proc_1.start()
+  proc_2.start()
+  proc_1.join()  #block the proc1 until terminates
+  proc_2.join()  #block the proc2 until terminates
