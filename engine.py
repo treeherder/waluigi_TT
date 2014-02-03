74b2bec5897d9fd251181a41b0ef8ef2fcb6fee1
treeherder <b.d.reddybest@gmail.com>
2014-02-02 22:30:59 -0800

    took out multiprocessing, too much to debug for now

--- engine.py
+++ engine.py
@@ -8,15 +8,8 @@ def strm():
 def prnt():
   c = sqlite3.connect('waluigi_tt.db')
   cur = c.cursor()
-  
   cur.execute("select * from tweets where rowid = (select max(rowid) from tweets)")
   r = cur.fetchone()
   print "content {0}".format(r)
 
-while True:
-  proc_1 =  Process(target=strm)
-  proc_2 = Process(target=prnt)
-  proc_1.start()
-  proc_2.start()
-  proc_1.join()  #block the proc1 until terminates
-  proc_2.join()  #block the proc2 until terminates
+strm()
