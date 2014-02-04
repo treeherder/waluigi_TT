76e23dd3aa33384ea7c8a1f6fcbc298131baefb3
treeherder <b.d.reddybest@gmail.com>
2014-02-04 01:55:55 -0800

    markov implementation
    markov.py from kernigan and pike’s perl

--- engine.py
+++ engine.py
@@ -5,11 +5,5 @@ from multiprocessing import Process
 def strm():
   stream.filter(track=['year luigi'])
 
-def prnt():
-  c = sqlite3.connect('waluigi_tt.db')
-  cur = c.cursor()
-  cur.execute("select * from tweets where rowid = (select max(rowid) from tweets)")
-  r = cur.fetchone()
-  print "content {0}".format(r)
 
 strm()
---
+++ markov.py
@@ -0,0 +1,46 @@
+
+# mostly taken from  Kernighan and Pike 'The Practice of Programming'
+
+import random
+
+stop = "\n" # Since we split on whitespace, this can never be a word
+sen_lim = (".", "!", "?",) # Cause a "new sentence" if found at the end of a word
+sen_sep  = "\n" #String used to seperate sentences
+
+
+# GENERATE TABLE
+first_stop = stop
+second_stop = stop
+table = {}
+
+
+for line in src_txt:
+    for word in line.split():
+        if word[-1] in sen_lim:
+            table.setdefault( (first_stop, second_stop), [] ).append(word[0:-1])
+            first_stop, second_stop = second_stop, word[0:-1]
+            word = word[-1]
+        table.setdefault( (first_stop, second_stop), [] ).append(word)
+        first_stop, second_stop = second_stop, word
+# Mark the end of the file
+table.setdefault( (first_stop, second_stop), [] ).append(stop)
+
+# GENERATE SENTENCE OUTPUT
+max_groups  = 5
+
+first_stop = stop
+second_stop = stop
+sen_count = 0
+sentence = []
+
+
+while sen_count < max_groups:
+    new_word = random.choice(table[(first_stop, second_stop)])
+    if new_word == stop: sys.exit()
+    if new_word in sen_lim:
+        print "%s%s%s" % (" ".join(sentence), new_word, sen_sep)
+        sentence = []
+        sen_count += 1
+    else:
+        sentence.append(new_word)
+    first_stop, second_stop = second_stop, new_word
